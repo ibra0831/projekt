8a277e56b989415f9ab7ee078579826bb4f523c9
Alexander <alexandersabroethomsen@gmail.com>
2024-12-06 12:36:19 +0100

    Grid opdateret

--- player_logic.js
+++ player_logic.js
@@ -1,6 +1,7 @@
 function initialize_player() {
     ctx = document.getElementById("game_canvas").getContext("2d");
 
-    ctx.fillStyle = "green";
+    ctx.fillStyle = "#47A025";
     ctx.fillRect(10,10, 150,100);
+
 }
